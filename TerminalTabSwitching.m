86a06804d34c24e8a95286277c663cb687c9c7a9
Yoshimasa Niwa <niw@niw.at>
2010-01-31 22:32:26 +0900

    Address the issue of weird behaviors on Snow Leopard

--- TerminalTabSwitching.m
+++ TerminalTabSwitching.m
@@ -7,6 +7,9 @@
 
 	for(NSMenuItem* menuItem in [windowsMenu itemArray])
 	{
+//		NSString* key = [menuItem keyEquivalent];
+//		if(([key length] == 1 && 0x30 <= [key characterAtIndex:0] && [key characterAtIndex:0] <= 0x39)
+//		 || [menuItem action] == @selector(selectRepresentedTabViewItem:))
 		if([menuItem action] == @selector(selectRepresentedTabViewItem:))
 			[windowsMenu removeItem:menuItem];
 	}
@@ -24,27 +27,30 @@
 	}
 }
 
-- (void)TerminalTabSwitching_windowDidBecomeMain:(id)fp8;
+- (void)TerminalTabSwitching_windowDidLoad
+{
+	// This method is not called at first time for the first main window.
+	[self TerminalTabSwitching_windowDidLoad];
+	[[NSApplication sharedApplication] removeWindowsItem:[self window]];
+	[[self window] setExcludedFromWindowsMenu:YES];
+	[self updateTabListMenu];
+}
+
+- (void)TerminalTabSwitching_windowDidBecomeMain:(id)fp8
 {
 	[self TerminalTabSwitching_windowDidBecomeMain:fp8];
 	[self updateTabListMenu];
 }
 
-- (void)tabViewDidChangeNumberOfTabViewItems:(NSTabView*)aTabView
+- (void)TerminalTabSwitching_newTab:(id)fp8
 {
+	[self TerminalTabSwitching_newTab:fp8];
 	[self updateTabListMenu];
 }
 
-- (void)TerminalTabSwitching_awakeFromNib;
+- (void)TerminalTabSwitching_closeTab:(id)fp8
 {
-	[[NSApplication sharedApplication] removeWindowsItem:[self window]];
-	[[self window] setExcludedFromWindowsMenu:YES];
-	[self TerminalTabSwitching_awakeFromNib];
-}
-
-- (void)TerminalTabSwitching_newTab:(id)fp8;
-{
-	[self TerminalTabSwitching_newTab:fp8];
+	[self TerminalTabSwitching_closeTab:fp8];
 	[self updateTabListMenu];
 }
 
@@ -61,9 +67,29 @@
 @implementation TerminalTabSwitching
 + (void)load
 {
-	[[[NSApplication sharedApplication] windowsMenu] addItem:[NSMenuItem separatorItem]];
 	[NSClassFromString(@"TTWindowController") jr_swizzleMethod:@selector(windowDidBecomeMain:) withMethod:@selector(TerminalTabSwitching_windowDidBecomeMain:) error:NULL];
-	[NSClassFromString(@"TTWindowController") jr_swizzleMethod:@selector(awakeFromNib) withMethod:@selector(TerminalTabSwitching_awakeFromNib) error:NULL];
-  [NSClassFromString(@"TTWindowController") jr_swizzleMethod:@selector(newTab:) withMethod:@selector(TerminalTabSwitching_newTab:) error:NULL];
+	[NSClassFromString(@"TTWindowController") jr_swizzleMethod:@selector(windowDidLoad) withMethod:@selector(TerminalTabSwitching_windowDidLoad) error:NULL];
+	[NSClassFromString(@"TTWindowController") jr_swizzleMethod:@selector(newTab:) withMethod:@selector(TerminalTabSwitching_newTab:) error:NULL];
+	[NSClassFromString(@"TTWindowController") jr_swizzleMethod:@selector(closeTab:) withMethod:@selector(TerminalTabSwitching_closeTab:) error:NULL];
+
+	NSApplication *app = [NSApplication sharedApplication];
+	NSWindow *mainWindow = [app mainWindow];
+
+	// NOTE The issue of timing to install SIMBL hook, we need to exclude the window already laoded here.
+	// After loaded, we can exclude new windows inside windowDidLoad callback method.
+	BOOL shouldExcludeMainWindow = ![mainWindow isExcludedFromWindowsMenu];
+
+	if(shouldExcludeMainWindow) {
+		[app removeWindowsItem:mainWindow];
+		[mainWindow setExcludedFromWindowsMenu:YES];
+	}
+
+	[[app windowsMenu] addItem:[NSMenuItem separatorItem]];
+
+	if(shouldExcludeMainWindow) {
+		[[mainWindow windowController] updateTabListMenu];
+	}
+
+	NSLog(@"TerminalTabSwitching installed");
 }
 @end
